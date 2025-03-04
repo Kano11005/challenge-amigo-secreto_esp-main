9140cb40ed476423c14c7d0624e31d2c9bf3a119
Kano11005 <kano11005@yahoo.com>
2025-03-04 15:02:01 -0300

    cambios

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 <h1>Challenge amigo Secreto</h1>
 
-Estado del Proyecto en contruccion
+Proyecto que permite selecionar un amigo secreto de un listado de amigos ingresados
 
-se empieza a trabajar en archivo app.js 
+el proyecto cuenta con 4 funciones principales
 
-crear array para almacenar nombres
+funcion agregarAmigo(); que permite ir agregando amigos que participaran en el amigo secreto
 
+funcion limpiartexto(); permite limpiar el campo nombre para que el usuario no tenga que borrar para agregar un nuevo nombre
+
+funcion listaAmigo(); permite listar todos los nombres de los amigos ingresados 
+
+funcion sortearAmigo(); permite seleccionar en forma automatica y aleatorea del listado de amigos el amigo secreto
+
+las funciones agregarAmigo() y sortearAmigo() son ejecutas desde un boton creado en el archivo index.html
--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let NombreAmigo = [];
+
+function agregarAmigo() {
+       let texto = document.getElementById("amigo").value;
+       if (texto == "") {
+          alert("Por favor, inserte un nombre.");
+          return;
+       } else {
+           NombreAmigo.push(texto);
+       }
+       limpiartexto();
+       listaAmigo();
+    
+}
+
+function limpiartexto() {
+    document.querySelector('#amigo').value = '';
+   
+}
+
+function listaAmigo() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < NombreAmigo.length; i++) {
+         let li = document.createElement("li"); // Crear elemento <li>
+         li.textContent = NombreAmigo[i]; // Asignar el nombre al <li>
+         lista.appendChild(li); // Agregar el <li> a la lista <ul>
+        }    
+ 
+}
+
+function sortearAmigo() {
+    let resultado = document.getElementById("resultado");
+    if (NombreAmigo.length === 0) {
+        resultado.innerHTML = "No hay amigos en la lista.";
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random() * NombreAmigo.length);
+    let amigoSorteado = NombreAmigo[indiceAleatorio];
+    resultado.innerHTML = `🎉 El amigo sorteado es: <strong>${amigoSorteado}</strong>`;
+}
